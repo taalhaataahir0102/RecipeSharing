7323d8ed1f4915d9138f501b859c14c17d9fbf2e
taalhaataahir0102 <23100293@lums.edu.pk>
2023-06-08 01:21:05 +0500

    gg2

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -34,6 +34,7 @@ const Dashboard = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({ postId }),
       });
